a74b08d5deee145a628abe3965b2276faffae240
kristenli <kyli2005@gmail.com>
2022-04-25 22:33:50 -0400

    commit tree 7aa68cbc7b9d73b0daaf9c37e926a4d64b6f16dd

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+const newArr = [];
+
+
+function writeCards(names, event){
+    for (let i = 0; i < names.length; i++){
+        
+    newArr.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+        debugger;
+    }
+    return newArr
+}
+
+function countDown(init = 10) {
+    while (init >= 0) {
+        console.log(init--);
+    }
+}
